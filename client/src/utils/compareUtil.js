d51d5b435f7957c1fd072f858193eddf00ccb922
Akash Patel <akashneo3@gmail.com>
2020-03-14 16:57:07 +0530

    corrected calculation of rank and rank change

--- client/src/utils/compareUtil.js
+++ client/src/utils/compareUtil.js
@@ -1,13 +1,19 @@
-function nthCompare(data, n = 2, term = "marketcap_rank", default_value = 0) {
+function nthCompare(
+  data,
+  n = 1,
+  n1 = 3,
+  term = "marketcap_rank",
+  default_value = 0
+) {
   let result = {
     diff: default_value,
     status: "-"
   };
   const tempData = Object.assign([], data).reverse();
-  if (tempData.length >= n+1) {
+  if (tempData.length >= n1) {
     const tempData1 = { data: tempData };
     const {
-      data: { [n]: nthData, [n - 1]: n1thData }
+      data: { [n - 1]: nthData, [n1 - 1]: n1thData }
     } = tempData1;
 
     let sum = n1thData[term] - nthData[term];
@@ -23,24 +29,30 @@ function nthCompare(data, n = 2, term = "marketcap_rank", default_value = 0) {
       };
     } else {
       result = {
-        diff: '--',
-        status: "--"
+        diff: "-",
+        status: "-"
       };
     }
   }
   return result;
 }
 
-function getNth(data, n = 2, term = "marketcap_rank", default_value = "--") {
+function getNth(
+  data,
+  n = 1,
+  n1 = 3,
+  term = "marketcap_rank",
+  default_value = "-"
+) {
   let result = {
     diff: default_value,
     status: "-"
   };
   const tempData = Object.assign([], data).reverse();
-  if (tempData.length >= n+1) {
+  if (tempData.length >= n1) {
     const tempData1 = { data: tempData };
     const {
-      data: { [n]: nthData, [n - 1]: n1thData }
+      data: { [n - 1]: nthData, [n1 - 1]: n1thData }
     } = tempData1;
 
     let sum = n1thData[term] - nthData[term];
@@ -57,7 +69,7 @@ function getNth(data, n = 2, term = "marketcap_rank", default_value = "--") {
     } else {
       result = {
         diff: nthData[term],
-        status: "--"
+        status: "-"
       };
     }
   }
